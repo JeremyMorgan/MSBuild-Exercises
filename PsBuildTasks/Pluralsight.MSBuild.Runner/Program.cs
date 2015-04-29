2cceede97a337812f9c60705a60b783fe3ee08bd
unknown <jmorgan@DEV01JMORG01.eidpassport.com>
2015-04-29 10:23:56 -0700

    adding project

---
+++ PsBuildTasks/Pluralsight.MSBuild.Runner/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Framework;
+using Pluralsight.MSBuild.Tasks;
+
+namespace Pluralsight.MSBuild.Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Add2Numbers ourAdder = new Add2Numbers();
+            ourAdder.Number1 = 2;
+            ourAdder.Number2 = 3;
+
+            Console.WriteLine("Result is:" + ourAdder.Result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PsBuildTasks/Pluralsight.MSBuild.Tests/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pluralsight.MSBuild;
+using Pluralsight.MSBuild.Tasks;
+
+
+namespace Pluralsight.MSBuild.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Add2Numbers ourAdder = new Add2Numbers();
+
+            ourAdder.Number1 = 3;
+            ourAdder.Number2 = 3;
+
+            double expected = 6;
+            
+            Debug.WriteLine("Result: " + ourAdder.Result);
+
+            Assert.AreEqual(ourAdder.Result, expected);
+
+        }
+    }
+}
